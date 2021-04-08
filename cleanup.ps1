787a4539f8319a20085e2a5563245c69310279e3
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2021-04-08 02:12:19 -0400

    Self elevate within the powershell script

--- cleanup.ps1
+++ cleanup.ps1
@@ -1,3 +1,12 @@
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -FilePath PowerShell.exe -ExecutionPolicy Bypass -Verb Runas -ArgumentList $CommandLine
+        Exit
+    }
+}
+
 Write-Output "Huion/Gaomon Cleanup Script"
 
 $pnputil = & pnputil -e
