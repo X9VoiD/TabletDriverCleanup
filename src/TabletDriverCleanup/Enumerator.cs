b1a02a1794f83d613da53fbe88a31299609a22da
X9VoiD <oscar.silvestrexx@gmail.com>
2023-02-09 23:55:53 +0800

    e

---
+++ src/TabletDriverCleanup/Enumerator.cs
@@ -0,0 +1,27 @@
+using System.Collections.Immutable;
+using Vanara.PInvoke;
+
+namespace TabletDriverCleanup;
+
+public static class Enumerator
+{
+    public static ImmutableArray<Device> GetDevices()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static ImmutableArray<Driver> GetDrivers()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Device
+{
+
+}
+
+public class Driver
+{
+
+}
