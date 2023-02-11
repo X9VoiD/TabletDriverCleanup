cbe7e2fe8a69e1dc2f474eb49e0f954be365058a
X9VoiD <oscar.silvestrexx@gmail.com>
2023-02-11 14:30:59 +0800

    Add helper bat files

--- build.ps1
+++ build.ps1
@@ -1,1 +1,4 @@
 dotnet publish .\src\TabletDriverCleanup\ /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugType=embedded --configuration=Release --self-contained=true -o ./build
+Copy-Item -Path ./eng/dump.bat -Destination ./build
+Copy-Item -Path ./eng/dry_run.bat -Destination ./build
+Copy-Item -Path ./eng/no_prompt_uninstall.bat -Destination ./build
