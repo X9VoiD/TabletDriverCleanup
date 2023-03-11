c6010078bc66e110fedc619f90942930456958b3
X9VoiD <oscar.silvestrexx@gmail.com>
2023-03-11 22:37:21 +0800

    chore: remove commented code

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -3,14 +3,3 @@ pub mod interest;
 pub mod regex_cache;
 pub mod terminal;
 pub mod windows;
-
-// pub struct ConfigurationManager {
-//     gh_url: String,
-//     gh_ref: String,
-// }
-
-// impl ConfigurationManager {
-//     pub fn get_config<T>(&self, string: &str) -> Result<T, String> {
-//         unimplemented!()
-//     }
-// }
